ccb7044ff809a291f6e096812d06a7f1f5776113
ozaswei <ozaswei@gmail.com>
2026-02-08 12:35:32 -0500

    tinkering with the  props and adding more functions to the ratings

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { useState } from "react";
 // import "./index.css";
 // import App from "./App";
 import StartRating from "./StartRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StartRating color="blue" onSetRating={setMovieRating} />
+      <p>Movie was rated {movieRating} stars </p>
+    </div>
+  );
+}
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StartRating maxRating={5} />
+    <StartRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
     {/* <StartRating maxRating={10} />
-    <StartRating /> */}
+     */}
+    <StartRating defaultRating={3} />
+    <Test />
   </React.StrictMode>,
 );
