47808399d137c124cdf39e0e75b28cceed8b0b42
ozaswei <ozaswei@gmail.com>
2026-03-09 12:03:26 -0400

    adding event listeners when we press esc and closing the movie when we search

--- src/App.js
+++ src/App.js
@@ -118,6 +118,7 @@ export default function App() {
         setError("");
         return;
       }
+      handleCloseMovie();
       fetchMovies();
 
       return function () {
@@ -294,6 +295,23 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     onAddWatched(newWatchedMovie);
     onCloseMovie();
   }
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (e.code === "Escape") {
+          onCloseMovie();
+        }
+      }
+      document.addEventListener("keydown", callback);
+
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [onCloseMovie],
+  );
+
   useEffect(
     function () {
       async function getMovieDetails() {
